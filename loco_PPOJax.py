f7a913ef3968b504042ecec657b0a13846008045
Kelvin <kelvintoonen2001@outlook.com>
2025-10-08 19:16:29 +0200

    Fix bugs and upload new run

--- loco_PPOJax.py
+++ loco_PPOJax.py
@@ -18,9 +18,18 @@ SERVER = False
 TRAIN = False
 
 
-@hydra.main(version_base=None, config_path="./", config_name="conf_MSM")
+@hydra.main(version_base=None, config_path="./", config_name="conf")
 def experiment(config: DictConfig):
     try:
+        if not SERVER:
+            training_steps = 1_000_000_000
+            algorithm = "PPO"
+            env_id = "MjxSkeletonMuscle"
+            model_name = algorithm + "_" + env_id + "_" + str(training_steps)
+            dir = f"./{model_name}/"
+            if not os.path.exists(model_name):
+                os.makedirs(model_name)
+
         if SERVER or TRAIN:
             os.environ['XLA_FLAGS'] = (
                 '--xla_gpu_triton_gemm_any=True ')
@@ -61,6 +70,49 @@ def experiment(config: DictConfig):
             save_path = PPOJax.save_agent(result_dir, agent_conf, agent_state)
             # run.config.update({"agent_save_path": save_path})
 
+            import time
+            t_start = time.time()
+            # get the metrics and log them
+            if not config.experiment.debug:
+                training_metrics = out["training_metrics"]
+                validation_metrics = out["validation_metrics"]
+
+                # calculate mean across seeds
+                training_metrics = jax.tree.map(lambda x: jnp.mean(jnp.atleast_2d(x), axis=0), training_metrics)
+                validation_metrics = jax.tree.map(lambda x: jnp.mean(jnp.atleast_2d(x), axis=0), validation_metrics)
+
+                for i in range(len(training_metrics.mean_episode_return)):
+                    print({"Mean Episode Return": training_metrics.mean_episode_return[i],
+                            "Mean Episode Length": training_metrics.mean_episode_length[i]},
+                            f"step={int(training_metrics.max_timestep[i])}")
+
+                    if (i+1) % config.experiment.validation_interval == 0 and config.experiment.validation.active:
+                        print({"Validation Info/Mean Episode Return": validation_metrics.mean_episode_return[i],
+                                    "Validation Info/Mean Episode Length": validation_metrics.mean_episode_length[i]},
+                                f"step={int(training_metrics.max_timestep[i])}")
+
+                        # log all measures
+                        metrics_to_log = {}
+                        for field in fields(validation_metrics):
+                            attr = getattr(validation_metrics, field.name)
+                            if isinstance(attr, QuantityContainer):
+                                measure_name = field.name
+                                for field_attr in fields(attr):
+                                    attr_name = field_attr.name
+                                    attr_value = getattr(attr, attr_name)
+                                    if attr_value.size > 0:
+                                        metrics_to_log[f"Validation Measures/{measure_name}/{attr_name}"] = attr_value[i]
+
+                        # run.log(metrics_to_log, step=int(training_metrics.max_timestep[i]))
+
+                        # metric for used for wandb sweep (optional)
+                        site_rpos = validation_metrics.euclidean_distance.site_rpos[i]
+                        site_rrotvec = validation_metrics.euclidean_distance.site_rpos[i]
+                        site_rvel = validation_metrics.euclidean_distance.site_rpos[i]
+                        # run.log({"Metric for Sweep": site_rpos + site_rrotvec + site_rvel},
+                        #         step=int(training_metrics.max_timestep[i]))
+
+            print(f"Time taken to log metrics: {time.time() - t_start}s")
 
         # load agent
         agent_conf, agent_state = PPOJax.load_agent(dir + "PPOJax_saved.pkl")
@@ -75,55 +127,10 @@ def experiment(config: DictConfig):
         config.experiment.env_params["headless"] = False
         env = factory.make(**config.experiment.env_params, **config.experiment.task_factory.params)
 
-
-        import time
-        t_start = time.time()
-        # get the metrics and log them
-        if not config.experiment.debug:
-            training_metrics = out["training_metrics"]
-            validation_metrics = out["validation_metrics"]
-
-            # calculate mean across seeds
-            training_metrics = jax.tree.map(lambda x: jnp.mean(jnp.atleast_2d(x), axis=0), training_metrics)
-            validation_metrics = jax.tree.map(lambda x: jnp.mean(jnp.atleast_2d(x), axis=0), validation_metrics)
-
-            for i in range(len(training_metrics.mean_episode_return)):
-                print({"Mean Episode Return": training_metrics.mean_episode_return[i],
-                        "Mean Episode Length": training_metrics.mean_episode_length[i]},
-                        f"step={int(training_metrics.max_timestep[i])}")
-
-                if (i+1) % config.experiment.validation_interval == 0 and config.experiment.validation.active:
-                    print({"Validation Info/Mean Episode Return": validation_metrics.mean_episode_return[i],
-                                "Validation Info/Mean Episode Length": validation_metrics.mean_episode_length[i]},
-                            f"step={int(training_metrics.max_timestep[i])}")
-
-                    # log all measures
-                    metrics_to_log = {}
-                    for field in fields(validation_metrics):
-                        attr = getattr(validation_metrics, field.name)
-                        if isinstance(attr, QuantityContainer):
-                            measure_name = field.name
-                            for field_attr in fields(attr):
-                                attr_name = field_attr.name
-                                attr_value = getattr(attr, attr_name)
-                                if attr_value.size > 0:
-                                    metrics_to_log[f"Validation Measures/{measure_name}/{attr_name}"] = attr_value[i]
-
-                    # run.log(metrics_to_log, step=int(training_metrics.max_timestep[i]))
-
-                    # metric for used for wandb sweep (optional)
-                    site_rpos = validation_metrics.euclidean_distance.site_rpos[i]
-                    site_rrotvec = validation_metrics.euclidean_distance.site_rpos[i]
-                    site_rvel = validation_metrics.euclidean_distance.site_rpos[i]
-                    # run.log({"Metric for Sweep": site_rpos + site_rrotvec + site_rvel},
-                    #         step=int(training_metrics.max_timestep[i]))
-
-        print(f"Time taken to log metrics: {time.time() - t_start}s")
-
         if not SERVER:
             # run the environment with the trained agent to record video
-            PPOJax.play_policy(env, agent_conf, agent_state, deterministic=True, n_steps=200, n_envs=20, record=True,
-                            train_state_seed=0)
+            PPOJax.play_policy(env, agent_conf, agent_state, deterministic=True, n_steps=1000, n_envs=20, record=True,
+                            train_state_seed=0, render=False)
             video_file = env.video_file_path
             print({"Save video to": video_file})
 
